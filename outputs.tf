80c0cdcb8ed316f9829441fffe1d365b609d6dc2
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:36:10 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "synapse_integration_runtime_self_hosteds" {
+  description = "All synapse_integration_runtime_self_hosted resources"
+  value       = azurerm_synapse_integration_runtime_self_hosted.synapse_integration_runtime_self_hosteds
+}
+
